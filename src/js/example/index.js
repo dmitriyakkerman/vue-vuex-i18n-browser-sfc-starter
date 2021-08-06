f9cc030d8e962897aa3d2ca935208d7f4cf12804
dmitriyakkerman <dmitriyakkerman@gmail.com>
2021-08-06 15:14:24 +0300

    vuex added

--- src/js/example/index.js
+++ src/js/example/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import {store} from "../store";
 import Example from "./components/Example.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
+    store,
     render: h => h(Example),
 }).$mount('.example');
